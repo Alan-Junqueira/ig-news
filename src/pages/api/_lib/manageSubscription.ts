78a1b968b23b3f4f07c64a73bf4b91d19f74c550
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-09-06 15:58:42 -0300

    Saving event data on db

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,39 @@
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+) {
+  // buscar o usuário no banco do FaunaDB com o ID {customerID}
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(
+        q.Match(
+          // Index = ao customerId
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  );
+
+  // .retrieve significa qeu quer bucar apenas uma.
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  };
+
+  // Salvar os dados da subscription do usuário no faunaDB
+  await fauna.query(
+    // Salvar, dentro da collection subscription os dados.
+    q.Create(q.Collection('subscriptions'), { data: subscriptionData })
+  );
+}
